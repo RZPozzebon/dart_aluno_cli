a61024abe6da32db6c7318db1f3cb1be6dd593c0
Rodrigo Pozzebon <rodrigo.pozzebon_desenv@hotmail.com>
2023-06-03 09:52:55 -0300

    Primeiro commit

---
+++ bin/dart_aluno_cli.dart
@@ -0,0 +1,5 @@
+import 'package:dart_aluno_cli/dart_aluno_cli.dart' as dart_aluno_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_aluno_cli.calculate()}!');
+}
---
+++ test/dart_aluno_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_aluno_cli/dart_aluno_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
